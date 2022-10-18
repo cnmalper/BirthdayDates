0bf398920d01ea72cdcfe7bc5c917ef0c2aff991
Alper Canimoglu <canimoglu185@gmail.com>
2022-10-18 12:10:47 +0300

    Initial Commit

---
+++ BirthdayDates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayDates
+//
+//  Created by Alper Canımoğlu on 18.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
